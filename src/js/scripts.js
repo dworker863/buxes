abc2aedc95140c0b9e6a21615c1d947e1d5a12a6
dworker <dworker863@gmail.com>
2021-05-30 22:31:13 +0600

    Added fontawesome and item with slider for russian section

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,4 +3,14 @@ $(document).ready(function () {
     $('.hamburger').toggleClass('is-active');
     $('.menu').slideToggle('slow');
   });
+  $('.fa-chevron-right').click(function () {
+    $(this).css('opacity', 0.5);
+    $('.fa-chevron-left').css('opacity', 1);
+    $('.slider__items').css({ transform: 'translateX(-310px)', transition: 'all, 0.5s' });
+  });
+  $('.fa-chevron-left').click(function () {
+    $(this).css('opacity', 0.5);
+    $('.fa-chevron-right').css('opacity', 1);
+    $('.slider__items').css({ transform: 'translateX(0)', transition: 'all, 0.5s' });
+  });
 });
